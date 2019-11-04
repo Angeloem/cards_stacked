7ca73cde0f8f15c5c57bd5c3c35b35aded4092bf
angeloem <esanga530@gmail.com>
2019-11-03 23:32:59 -0500

    doing slideshow via css

---
+++ scripting.js
@@ -0,0 +1,11 @@
+let topp = document.querySelector("#_2");
+let land_rover = document.querySelector("_1");
+let deezer = document.querySelector('_3');
+
+
+topp.addEventListener('click', function () {
+    console.log('yeeey')
+});
+topp.addEventListener('drag', function () {
+    console.log('yaaay')
+});
