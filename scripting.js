ca231974c82e0374915300888307d96e7a126ed9
angeloem <esanga530@gmail.com>
2019-11-05 13:49:49 -0500

    the cards can now be exchanged via click.. game almost over,, the method tracks only three cards, got to add some more logic to include more innumerable cards to go with

--- scripting.js
+++ scripting.js
@@ -1,11 +1,31 @@
 let topp = document.querySelector("#_2");
-let land_rover = document.querySelector("_1");
-let deezer = document.querySelector('_3');
+let land_rover = document.querySelector("#_1");
+let deezer = document.querySelector('#_3');
 
 
 topp.addEventListener('click', function () {
-    console.log('yeeey')
+    topp.style.left = '90px';
+    topp.style.zIndex = '1';
+    land_rover.style.left = '0px';
+    land_rover.style.zIndex = '3';
+    deezer.style.zIndex = '2';
+    deezer.style.left = '50px';
 });
-topp.addEventListener('drag', function () {
-    console.log('yaaay')
+
+land_rover.addEventListener('click', function () {
+    topp.style.left = '50px';
+    topp.style.zIndex = '2';
+    land_rover.style.left = '90px';
+    land_rover.style.zIndex = '1';
+    deezer.style.left = '0px';
+    deezer.style.zIndex = '3';
 });
+
+deezer.addEventListener('click', function () {
+    topp.style.left = '0';
+    topp.style.zIndex = '3';
+    land_rover.style.left = '50px';
+    land_rover.style.zIndex = '2';
+    deezer.style.left = '90px';
+    deezer.style.zIndex = '1';
+})
