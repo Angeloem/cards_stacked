3be344664d724dc0f0ca6f379be7b974ea59a51d
angeloem <esanga530@gmail.com>
2019-11-05 13:52:05 -0500

    added an if statement to check whether the clicked card is the top card or else

--- scripting.js
+++ scripting.js
@@ -4,28 +4,34 @@ let deezer = document.querySelector('#_3');
 
 
 topp.addEventListener('click', function () {
-    topp.style.left = '90px';
-    topp.style.zIndex = '1';
-    land_rover.style.left = '0px';
-    land_rover.style.zIndex = '3';
-    deezer.style.zIndex = '2';
-    deezer.style.left = '50px';
+    if(topp.style.left === '0px') {
+        topp.style.left = '90px';
+        topp.style.zIndex = '1';
+        land_rover.style.left = '0px';
+        land_rover.style.zIndex = '3';
+        deezer.style.zIndex = '2';
+        deezer.style.left = '50px';
+    }
 });
 
 land_rover.addEventListener('click', function () {
-    topp.style.left = '50px';
-    topp.style.zIndex = '2';
-    land_rover.style.left = '90px';
-    land_rover.style.zIndex = '1';
-    deezer.style.left = '0px';
-    deezer.style.zIndex = '3';
+    if (land_rover.style.left === '0px') {
+        topp.style.left = '50px';
+        topp.style.zIndex = '2';
+        land_rover.style.left = '90px';
+        land_rover.style.zIndex = '1';
+        deezer.style.left = '0px';
+        deezer.style.zIndex = '3';
+    }
 });
 
 deezer.addEventListener('click', function () {
-    topp.style.left = '0';
-    topp.style.zIndex = '3';
-    land_rover.style.left = '50px';
-    land_rover.style.zIndex = '2';
-    deezer.style.left = '90px';
-    deezer.style.zIndex = '1';
-})
+    if(deezer.style.left === '0px') {
+        topp.style.left = '0';
+        topp.style.zIndex = '3';
+        land_rover.style.left = '50px';
+        land_rover.style.zIndex = '2';
+        deezer.style.left = '90px';
+        deezer.style.zIndex = '1';
+    }
+});
